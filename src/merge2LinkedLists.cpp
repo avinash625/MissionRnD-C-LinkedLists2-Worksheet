966af569779f4b602255108cb8ae9bdea0205069
avinash625 <avinash625@missionrnd.com>
2016-01-17 23:27:27 +0530

    Submit Commit

--- src/merge2LinkedLists.cpp
+++ src/merge2LinkedLists.cpp
@@ -19,5 +19,52 @@ struct node {
 };
 
 struct node * merge2LinkedLists(struct node *head1, struct node *head2) {
-	return NULL;
+	struct node *iterator1, *new_head = NULL , *iterator2, *temp;
+	if (head1 == NULL)
+		return head2;
+	else if (head2 == NULL)
+		return head1;
+	else if (head1 == NULL && head2 == NULL)
+		return NULL;
+	else
+	{
+		for (iterator1 = head1, iterator2 = head2; iterator1 != NULL && iterator2 != NULL; )
+		{
+			if (iterator1->num >= iterator2->num)
+			{
+				if (new_head == NULL)
+				{
+					temp = new_head = iterator2;
+				}
+				else
+				{
+					temp->next = iterator2;
+					temp = iterator2;
+				}
+				iterator2 = iterator2->next;
+			}
+			else
+			{
+				if (new_head == NULL)
+				{
+					temp = new_head = iterator1;
+				}
+				else
+				{
+					temp->next = iterator1;
+					temp = iterator1;
+				}
+				iterator1 = iterator1->next;
+			}
+		}
+		if (iterator1 != NULL)
+		{
+			for (; iterator1 != NULL; temp = iterator1, iterator1 = iterator1->next)
+				temp->next = iterator1;
+		}
+		else
+			for (; iterator2 != NULL;temp = iterator2, iterator2 = iterator2->next)
+				temp->next = iterator2;
+	}
+	return new_head;
 }
